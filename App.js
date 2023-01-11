e1711f9978c2d0b046dbea616daeb431ff863548
Brandon Holman <bholmdev@gmail.com>
2023-01-11 15:16:46 -0600

    Styles

--- App.js
+++ App.js
@@ -2,15 +2,15 @@ import { StyleSheet, SafeAreaView, Alert, Button } from 'react-native';
 
 export default function App() {
     return (
-        <SafeAreaView style={styles.container}>
+        <SafeAreaView style={[styles.container, containerStyle]}>
             <Button
-                color="orange"
+                color="blue"
                 title="Click Me"
                 onPress={() => Alert.prompt("My title", "My message", text => console.log(text))}/>
         </SafeAreaView>
     );
 }
-
+const containerStyle = { backgroundColor: "orange" }
 const styles = StyleSheet.create({
     container: {
         flex: 1,
