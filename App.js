979afcf3fc5b04cab2deffa1527e62f2b3403228
Brandon Holman <bholmdev@gmail.com>
2023-01-11 15:59:44 -0600

    flex

--- App.js
+++ App.js
@@ -1,35 +1,40 @@
-import {
-    Dimensions,
-    StyleSheet,
-    Platform,
-    SafeAreaView,
-    View,
-    StatusBar 
-} from 'react-native';
-import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function App() {
-    const {landscape} = useDeviceOrientation()
-
     return (
-        <SafeAreaView style={styles.container}>
+       <View
+            style={{
+                backgroundColor: "#fff",
+                flex: 1
+            }}
+       >
             <View
                 style={{
                     backgroundColor: "dodgerblue",
-                    width: "100%",
-                    height: landscape ? "100%" : "30%"
+                    flex: 2
                 }}
-            >
-
-            </View>
-        </SafeAreaView>
+            />
+            <View
+                style={{
+                    backgroundColor: "gold",
+                    flex: 1
+                }}
+            />
+            <View
+                style={{
+                    backgroundColor: "tomato",
+                    flex: 1
+                }}
+            />
+       </View> 
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+        backgroundColor: "dodgerblue",
+        alignItems: "center",
+        justifyContent: "center"
     },
 });
