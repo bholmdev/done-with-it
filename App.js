d62f7db6bc0b21430d43f34b60dd77a4c0f12bed
Brandon Holman <bholmdev@gmail.com>
2023-01-11 16:16:00 -0600

    justify and align

--- App.js
+++ App.js
@@ -6,21 +6,24 @@ export default function App() {
             style={{
                 backgroundColor: "#fff",
                 flex: 1,
-                flexDirection: "row"
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center"
             }}
        >
             <View
                 style={{
                     backgroundColor: "dodgerblue",
                     width: 100,
-                    height: 100
+                    height: 300,
+                    alignSelf: "flex-start"
                 }}
             />
             <View
                 style={{
                     backgroundColor: "gold",
                     width: 100,
-                    height: 100
+                    height: 200
                 }}
             />
             <View
