5a2de6cb1ab0bb5dc1eac77641eb15de242852ef
Brandon Holman <bholmdev@gmail.com>
2023-01-11 16:03:29 -0600

    flex direction

--- App.js
+++ App.js
@@ -5,25 +5,29 @@ export default function App() {
        <View
             style={{
                 backgroundColor: "#fff",
-                flex: 1
+                flex: 1,
+                flexDirection: "row"
             }}
        >
             <View
                 style={{
                     backgroundColor: "dodgerblue",
-                    flex: 2
+                    width: 100,
+                    height: 100
                 }}
             />
             <View
                 style={{
                     backgroundColor: "gold",
-                    flex: 1
+                    width: 100,
+                    height: 100
                 }}
             />
             <View
                 style={{
                     backgroundColor: "tomato",
-                    flex: 1
+                    width: 100,
+                    height: 100
                 }}
             />
        </View> 
