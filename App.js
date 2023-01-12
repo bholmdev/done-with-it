a6a5adc6a587b14727ffe0e907c934ce5a48b8b4
Brandon Holman <bholmdev@gmail.com>
2023-01-12 12:18:27 -0600

    Icons

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { Text, View } from 'react-native';
 import ViewImageScreen from './screens/ViewImageScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import AppText from './app/components/AppText';
+import { MaterialCommunityIcons } from "@expo/vector-icons"
 
 export default function App() {
     return (
@@ -12,9 +13,7 @@ export default function App() {
                 alignItems: "center"
             }}
         >
-            <AppText>
-                I love React Native!
-            </AppText>
+            <MaterialCommunityIcons name="email" size={200} color="dodgerblue" />
         </View>
     );
 };
