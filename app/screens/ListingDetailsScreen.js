3ca65eef162ecddde8d8d091e3eb945d8cd759d2
Brandon Holman <bholmdev@gmail.com>
2023-01-12 18:10:47 -0600

    Building the List Item Component

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Image, View } from 'react-native';
 import AppText from "../components/AppText/AppText.js";
 import colors from "../config/colors";
+import ListItem from '../components/ListItem.js';
 
 function ListingDetailsScreen(props) {
     return (
@@ -9,6 +10,13 @@ function ListingDetailsScreen(props) {
             <View style={styles.detailsContainer}>
                 <AppText style={styles.title}>Red jacket for sale</AppText>
                 <AppText style={styles.price}>$100</AppText>
+                <View style={styles.userContainer}>
+                    <ListItem
+                        image={require("../assets/mosh.jpg")}
+                        title="Mosh Hamedani"
+                        subTitle="5 Listings"
+                    />
+                </View>
             </View>
         </View>
     );
@@ -31,6 +39,9 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 24,
         fontWeight: "500"
+    },
+    userContainer: {
+        marginVertical: 40
     }
 });
 
