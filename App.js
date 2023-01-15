5c432342108bfb53d499eae796adbe648bb3d707
Brandon Holman <bholmdev@gmail.com>
2023-01-15 07:13:02 -0600

    Building the Icon Component

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
 import MessagesScreen from "./app/screens/MessagesScreen";
 import "react-native-gesture-handler"
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
 
 export default function App() {
     return (
-        <MessagesScreen />
+        <Screen>
+            <Icon
+                name="email"
+                size={100}
+                backgroundColor="red"
+                iconColor="white"
+            />
+        </Screen>
     );
 };
