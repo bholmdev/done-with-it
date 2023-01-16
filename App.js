8fed6375291ae2a4a9cd25a0e058e9dabefa93b8
Brandon Holman <bholmdev@gmail.com>
2023-01-16 13:20:17 -0600

    Building a Pretty Text Input

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import "react-native-gesture-handler";
 import Screen from ".//app/components/Screen";
 import { useState } from "react";
 import { Text, TextInput } from "react-native";
+import AppTextInput from "./app/components/AppTextInput";
 
 
 export default function App() {
@@ -9,19 +10,7 @@ export default function App() {
 
     return (
         <Screen>
-            <Text>{firstName}</Text>
-            <TextInput
-                secureTextEntry
-                clearButtonMode="always"
-                maxLength={5}
-                keyboardType="numeric"
-                onChangeText={text => setFirstName(text)}
-                placeholder="First Name"
-                style={{
-                    borderBottomColor: "#ccc",
-                    borderBottomWidth: 1
-                }}
-            />
+            <AppTextInput placeholder="Username" icon="email" />
         </Screen>
     );
 };
