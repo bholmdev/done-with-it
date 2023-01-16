8440d7beef6f07a6e11334776320dc650c1fa5b4
Brandon Holman <bholmdev@gmail.com>
2023-01-16 13:52:53 -0600

    Building a Custom Picker

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import { Switch } from "react-native";
 import "react-native-gesture-handler";
 import Screen from ".//app/components/Screen";
 import { useState } from "react";
+import AppPicker from "./app/components/AppPicker";
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
     const [isNew, setIsNew] = useState(false);
 
     return (
         <Screen>
-            <Switch value={isNew} onValueChange={(newValue) => setIsNew(newValue)} />
+            <AppPicker icon="apps" placeholder={"Category"} />
+            <AppTextInput icon="email" placeholder="email" />
         </Screen>
     );
 };
