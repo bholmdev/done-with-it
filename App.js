6b2783261d8ae882695ec89e43253c7afa239255
Brandon Holman <bholmdev@gmail.com>
2023-01-16 11:52:00 -0600

    Text Input

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
-import "react-native-gesture-handler"
-import ListingsScreen from "./app/screens/ListingsScreen";
+import "react-native-gesture-handler";
+import Screen from ".//app/components/Screen";
+import { Text, TextInput } from "react-native";
+import { useState } from "react";
 
 
 export default function App() {
+    const [firstName, setFirstName] = useState("");
+
     return (
-        <ListingsScreen />
+        <Screen>
+            <Text>{firstName}</Text>
+            <TextInput
+                secureTextEntry
+                clearButtonMode="always"
+                keyboardType="numeric"
+                onChangeText={text => setFirstName(text)}
+                placeholder="First Name"
+                style={{
+                    borderBottomColor: "#ccc",
+                    borderBottomWidth: 1
+                }}
+            />
+        </Screen>
     );
 };
