e4fb2107873da9b37454e58be2992bbc4d16e67a
Brandon Holman <bholmdev@gmail.com>
2023-01-18 07:38:31 -0600

    Building the SubmitButton Component

---
+++ app/components/AppFormField.js
@@ -0,0 +1,23 @@
+import AppTextInput from "./AppTextInput";
+import ErrorMessage from "./ErrorMessage";
+import { useFormikContext } from "formik";
+
+const AppFormField = ({ name, ...otherProps }) => {
+    const {setFieldTouched, handleChange, errors, touched} = useFormikContext();
+
+    return (
+        <>
+            <AppTextInput
+                onBlur={() => setFieldTouched(name)}
+                onChangeText={handleChange(name)}
+                {...otherProps}
+            />
+            <ErrorMessage
+                error={errors[name]}
+                visible={touched[name]}
+            />
+        </>
+    )
+};
+
+export default AppFormField;
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,10 +1,9 @@
 import { Image, StyleSheet } from "react-native";
 import Screen from "../components/Screen";
-import AppTextInput from "../components/AppTextInput";
 import AppButton from "../components/AppButton";
 import { Formik } from "formik";
 import * as Yup from "yup";
-import ErrorMessage from "../components/ErrorMessage";
+import AppFormField from "../components/AppFormField";
 
 const validationSchema = Yup.object().shape({
     email: Yup.string().required().email().label("Email"),
@@ -25,33 +24,23 @@ const LoginScreen = (props) => {
             >
                 {({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
                     <>
-                        <AppTextInput
+                        <AppFormField
                             autoCapitalize="none"
                             autoCorrect={false}
                             icon="email"
                             keyboardType="email-address"
-                            onBlur={() => setFieldTouched("email")}
-                            onChangeText={handleChange("email")}
+                            name="email"
                             placeholder="Email"
                             textContentType="emailAddress"
                         />
-                        <ErrorMessage
-                            error={errors.email}
-                            visible={touched.email}    
-                        />
-                        <AppTextInput
+                        <AppFormField
                             autoCapitalize="none"
                             autoCorrect={false}
                             icon="lock"
-                            onBlur={() => setFieldTouched("password")}
-                            onChangeText={handleChange("password")}
+                            name="password"
                             placeholder="Password"
                             secureTextEntry
                             textContentType="password"
-                        />
-                        <ErrorMessage
-                            error={errors.password}
-                            visible={touched.password}
                         />
                         <AppButton
                             title="Login"
